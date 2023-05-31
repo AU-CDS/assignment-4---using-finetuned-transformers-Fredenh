896fb063946ee04a6f547ee818671c6dee7d03fe
Frederik Normann Holm <112377991+Fredenh@users.noreply.github.com>
2023-05-31 02:18:48 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
-
 # Create virtual enviroment 
 python3 -m venv assignment_4
 
-# Activate virtual enviroment 
-#source ./assignment4/bin/activate 
-
-# install hdbscan for BERTopic
-#sudo apt-get update
-#sudo apt-get install python3-dev
-
-# requirements
-#pip3 install transformers
+# Install packages
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-
-# Deactivate the virtual environment.
-#deactivate
